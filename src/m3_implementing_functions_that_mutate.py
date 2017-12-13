25a7cd3a7cfa287e23a6639814518cb7396effa1
fusarocr <fusarocr@rose-hulman.edu>
2017-12-13 15:10:03 -0500

    what is temp how does it work

--- src/m3_implementing_functions_that_mutate.py
+++ src/m3_implementing_functions_that_mutate.py
@@ -3,8 +3,8 @@ This module lets you practice IMPLEMENTING
 functions that MUTATE their arguments.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Christopher Fusaro.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -146,6 +146,9 @@ def swap_colors(circle, rectangle):
     ####################################################################
     # ------------------------------------------------------------------
 
+    temp = circle.fill_color
+    circle.fill_color = rectangle.fill_color
+    rectangle.fill_color = temp
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
